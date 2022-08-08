731282de71a6649cdc107481e48bfed1406ce2f2
teagueStockwell <tsAppDevelopment@gmail.com>
2022-08-07 18:43:37 -0700

    Add some more css

--- features/connection/connection-view.tsx
+++ features/connection/connection-view.tsx
@@ -4,10 +4,9 @@ import { connectionStore, connectionActions } from "./connection-store";
 
 export const Nav = () => {
   const s = connectionStore((s) => s);
-  const [c, cc] = React.useState(0);
 
   return (
-    <div className={cn.navRoot}>
+    <div className={cn.topNavRoot}>
       <pre>
         {JSON.stringify(
           {
@@ -27,27 +26,39 @@ export const ConnectionView = (): JSX.Element => {
 
   if (s.status === "enter-self-id") {
     return (
-      <div className={cn.root}>
-        <label>enter your id</label>
-        <input value={s.selfId} onChange={connectionActions.setSelfId} />
-        <button onClick={connectionActions.publishToBroker}>done</button>
+      <div className={cn.bottomNavRoot}>
+        <div className={cn.col}>
+          <label>enter your id</label>
+          <input
+            className={cn.t}
+            value={s.selfId}
+            onChange={connectionActions.setSelfId}
+          />
+        </div>
+        <button onClick={connectionActions.publishToBroker}>next</button>
       </div>
     );
   }
 
   if (s.status === "connecting-self") {
     return (
-      <div className={cn.root}>
-        <span>publishing your id</span>
+      <div className={cn.bottomNavRoot}>
+        <span>publishing your id...</span>
       </div>
     );
   }
 
   if (s.status == "awaiting-peer") {
     return (
-      <div className={cn.root}>
-        <label>enter peer id</label>
-        <input value={s.peerId} onChange={connectionActions.setPeerId} />
+      <div className={cn.bottomNavRoot}>
+        <div className={cn.col}>
+          <label>enter peers id</label>
+          <input
+            className={cn.t}
+            value={s.peerId}
+            onChange={connectionActions.setPeerId}
+          />
+        </div>
         <button onClick={connectionActions.callPeer}>connect</button>
       </div>
     );
@@ -55,24 +66,34 @@ export const ConnectionView = (): JSX.Element => {
 
   if (s.status === "connecting-peer") {
     return (
-      <div className={cn.root}>
-        <span>connecting to peer</span>
+      <div className={cn.bottomNavRoot}>
+        <span>connecting to peer...</span>
       </div>
     );
   }
 
   if (s.status === "connected") {
     return (
-      <div className={cn.root}>
-        <label>enter a msg</label>
-        <input value={s.msg} onChange={connectionActions.setMsg} />
-        <button onClick={connectionActions.emit}>send</button>
-        {s.msgs.map((m) => (
-          <pre key={m.createdAt + m.receiverId}>
-            {JSON.stringify(m, null, 2)}
-          </pre>
-        ))}
-      </div>
+      <>
+        <div className={cn.bottomNavRoot}>
+          <textarea
+            className={cn.text}
+            value={s.msg}
+            onChange={connectionActions.setMsg}
+          />
+          <button onClick={connectionActions.emit}>send</button>
+        </div>
+        <div className={cn.msgsRoot}>
+          {s.msgs.map((m) => (
+            <pre
+              key={m.createdAt + m.receiverId}
+              style={{ alignSelf: m.senderId === s.selfId ? "flex-end" : "flex-start" }}
+            >
+              {JSON.stringify(m, null, 2)}
+            </pre>
+          ))}
+        </div>
+      </>
     );
   }
 
